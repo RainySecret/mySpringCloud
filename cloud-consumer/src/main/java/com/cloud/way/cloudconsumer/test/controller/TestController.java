8a11cd9dd5a220371e9842d4c68e4366a467f45d
chongwei <1402992457@qq.com>
2022-08-20 08:55:19 +0800

    添加依赖和代码优化

--- cloud-consumer/src/main/java/com/cloud/way/cloudconsumer/test/controller/TestController.java
+++ cloud-consumer/src/main/java/com/cloud/way/cloudconsumer/test/controller/TestController.java
@@ -1,11 +1,14 @@
 package com.cloud.way.cloudconsumer.test.controller;
 
 import com.cloud.way.cloudconsumer.test.service.TestService;
+import com.google.common.util.concurrent.RateLimiter;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author chongwei
@@ -17,11 +20,16 @@ import org.springframework.web.bind.annotation.RestController;
 @Log
 public class TestController {
 
+    private static final RateLimiter rateLimiter = RateLimiter.create(10);
+
     @Autowired
     private TestService testService;
 
     @GetMapping("/hello")
     public String hello(){
+        if (!rateLimiter.tryAcquire(0, TimeUnit.SECONDS)) {
+            return "访问过于频繁，请稍后重试";
+        }
         try {
             log.info("开始调用远程服务");
             return testService.hello();
